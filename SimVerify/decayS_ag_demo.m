1e177cf6d9f1ae5331910fa295f6586de68f146e
yzhizai <duansf@ihep.ac.cn>
2016-11-11 16:20:35 +0800

    add an angle

--- SimVerify/decayS_ag_demo.m
+++ SimVerify/decayS_ag_demo.m
@@ -1,9 +1,8 @@
-function decayS_pv_demo(theta)
-%theta - the angle between two fibers.
+function decayS_ag_demo(lambda)
 
 outputDir = spm_select(1, 'dir');
 rng('default')
-tensorDir1 = orth(rand(3)); % generate 3 direction.
+tensorDir1 = orth(rand(3));% generate 3 direction.
 
 bvalfile = spm_select(1, 'bval', ...
     'choose the diffusion direction');
@@ -12,35 +11,35 @@ bvecfile = spm_select(1, 'bvec', ...
     'choose the diffusion direction');
 bvec = load(bvecfile);
 
-Rvec = theta*tensorDir1(:, 3)';
-Rmat = rotationVectorToMatrix(Rvec); %make the tensorDir rotate with z-axis with 90 to
-%produce tensorDir2;
-
-tensorDir2 = Rmat*tensorDir1;
-
 S1 = decayS(tensorDir1, bval, bvec);
-S2 = decayS(tensorDir2, bval, bvec);
-
 S0 = 300;
 
-for aa = 0.5:0.1:0.9
-    fatherfolder = fullfile(outputDir, sprintf('lamba_%02d', round(aa*100)));
+
+for aa = pi/12:pi/12:pi/2
+    
+    fatherfolder = fullfile(outputDir, sprintf('theta_%02d', round(aa*180/pi)));
     mkdir(fatherfolder);
     copyfile(bvecfile, fullfile(fatherfolder, 'bvecs'));
     copyfile(bvalfile, fullfile(fatherfolder, 'bvals'));
-        
-    lambda = normrnd(aa, 0.05, 10, 10, 10); % sigma was defined as 0.05 to reduce the power.
-    decF = arrayfun(@(x) S0*(x*S1 + (1 - x)*S2), lambda, 'UniformOutput', false);
-    
+     
+    theta = normrnd(aa, pi/180, 10, 10, 10);
+    decF = arrayfun(@(x) decF_angle(x, tensorDir1, bval, bvec, lambda, S1, S0), ...
+        theta, 'UniformOutput', false);
+  
     %reformat the 3D cell to 4D array, first 3 dimensions was equalvalent, the 4 dimension was the cell{xx}(:)  
     temp = cellfun(@(x) reshape(x, 1, 1, 1, []), decF, 'UniformOutput', false);
     S_temp = cat(1, temp{:});
     S = reshape(S_temp, 10, 10, 10, []);
-    outputFiles(fatherfolder, S); 
+    outputFiles(fatherfolder, S);   
 end
 
-outputFiles('.', S);
-
+function S = decF_angle(x, tensorDir1, bval, bvec, lambda, S1, S0)
+Rvec = normrnd(x, pi/180)*tensorDir1(:, 3)';
+Rmat = rotationVectorToMatrix(Rvec); %make the tensorDir rotate with z-axis with 90 to
+%produce tensorDir2;
+tensorDir2 = Rmat*tensorDir1;
+S2 = decayS(tensorDir2, bval, bvec);
+S = S0*(lambda*S1 + (1 - lambda)*S2);
 
 %output simulated data.
 function outputFiles(dirname, S)
@@ -67,10 +66,8 @@ ni_mask.dat = file_array(fname, [xx, yy, zz], [16, spm_platform('bigend')],...
 ni_mask.mat = eye(4);
 ni_mask.mat0 = eye(4);
 ni_mask.descrip = 'mask data';
-
 create(ni_mask);    
 ni_mask.dat(:, :, :) = ones(xx, yy, zz);
-
 
 
 
