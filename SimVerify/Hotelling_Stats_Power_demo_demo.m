102f968db20d62f7931fdc81f828d8b29cdc14dc
yzhizai <duansf@ihep.ac.cn>
2016-11-13 22:52:45 +0800

    add a complete edition for hotelling stats

---
+++ SimVerify/Hotelling_Stats_Power_demo_demo.m
@@ -0,0 +1,16 @@
+function Hotelling_Stats_Power_demo_demo
+dirname = spm_select(1, 'dir');
+
+filenames = cellstr(spm_select(Inf, 'mat'));
+siT1 = load(filenames{1});
+
+Niter = 1000;
+T2 = zeros(numel(filenames) - 1, Niter);
+for aa = 2:numel(filenames)
+    siT2 = load(filenames{aa});   
+    for bb = 1:1000
+        T2(aa - 1,bb) = Hotelling_Stats_Power(siT1, siT2);
+    end
+end
+
+save(fullfile(dirname, 'T2.mat'), 'T2');
---
+++ SimVerify/powerplot.m
@@ -0,0 +1,34 @@
+function powerplot
+%%
+close all
+myval = cat(1, zeros(1, 1000), T2);
+% x =[3/2, 7/3, 4/1, 9];
+x = 15:15:90;
+figure
+hold on
+for aa = 1:size(myval, 2)
+    plot(x, myval(:, aa), 'g-');
+end
+plot(x, mean(myval, 2), 'b-o', 'LineWidth', 1)
+xlabel('partial volume');
+ylabel('F value');
+
+watpower = sum(myval > 19.5, 2)/1000;
+figure
+plot(x, watpower(:), 'b-o');
+hold on
+plot(x, ones(1, 6)*0.8, '--r')
+xlabel('partial volume')
+ylabel('Power');
+ylim([-0.2, 1.2]);
+hold off
+%%
+x = 15:15:90;
+watPower = [0, ones(1, 5)];
+figure;
+plot(x, watPower, 'b-o');
+hold on
+plot(x, ones(1, 6)*0.8, '--r')
+ylim([-0.2, 1.2]);
+xlabel('Angle')
+ylabel('Power');
